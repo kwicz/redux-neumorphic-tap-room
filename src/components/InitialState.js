abc55bf4af364e5eda3b89a6c0c737ee1aa970d4
Katy Solovewicz <katy@solovewicz.com>
2020-05-01 13:46:56 -0700

    add Initial State file and include it in the redux store

---
+++ src/components/InitialState.js
@@ -0,0 +1,62 @@
+const masterKegList = [
+  {
+    name: "Banana Belgium Ale",
+    brand: "Solovewicz Brewing",
+    description: "Fruity, but in a canine kind of way.",
+    alcoholContent: "4%",
+    price: 5,
+    remainingPints: 0,
+    key: 1,
+    id: 1
+  },
+  {
+    name: "Sneaky Sasquatch Stout",
+    brand: "Bigfoot Fermentorium",
+    description: "Pungent.",
+    alcoholContent: "8%",
+    price: 11,
+    remainingPints: 124,
+    key: 2,
+    id: 2
+  },
+  {
+    name: "Calvin & Hobbes Pale Ale",
+    brand: "Watterson Werks",
+    description: "Blonde with notes of childhood.",
+    alcoholContent: "5%",
+    price: 6,
+    remainingPints: 124,
+    key: 3,
+    id: 3
+  },
+  {
+    name: "Goat Lords Cider",
+    brand: "Wild Animal Werks",
+    description: "Maaahhhhy favorite cider.",
+    alcoholContent: "4%",
+    price: 5,
+    remainingPints: 124,
+    key: 4,
+    id: 4
+  },
+  {
+    name: "Dinosaur Double IPA",
+    brand: "Limited Edition.  Try it before it goes exctinct.",
+    description: "RAWR.",
+    alcoholContent: "9%",
+    price: 8,
+    remainingPints: 124,
+    key: 5,
+    id: 5
+  },
+  {
+    name: "Mary Poppins Kombucha",
+    brand: "Sister Suffragettes, inc.",
+    description: "Nearly perfect in every way.",
+    alcoholContent: "3%",
+    price: 7,
+    remainingPints: 124,
+    key: 6,
+    id: 6
+  }
+]
--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
+import InitialState from './components/InitialState';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  InitialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 store.subscribe(() =>
   console.log(store.getState())
