d71bd14f7b88e4d453c05d85fb0d218aa22b2b16
Katy Solovewicz <katy@solovewicz.com>
2020-05-01 14:04:27 -0700

    add uuid to Initial State

--- src/components/InitialState.js
+++ src/components/InitialState.js
@@ -1,62 +1,71 @@
-const masterKegList = [
-  {
+import { v4 } from 'uuid';
+
+const id1 = v4();
+const id2 = v4();
+const id3 = v4();
+const id4 = v4();
+const id5 = v4();
+const id6 = v4();
+
+export default masterKegList = {
+  [id1]: {
     name: "Banana Belgium Ale",
     brand: "Solovewicz Brewing",
     description: "Fruity, but in a canine kind of way.",
     alcoholContent: "4%",
     price: 5,
     remainingPints: 0,
-    key: 1,
-    id: 1
+    key: id1,
+    id: id1
   },
-  {
+  [id2]: {
     name: "Sneaky Sasquatch Stout",
     brand: "Bigfoot Fermentorium",
     description: "Pungent.",
     alcoholContent: "8%",
     price: 11,
     remainingPints: 124,
-    key: 2,
-    id: 2
+    key: id2,
+    id: id2
   },
-  {
+  [id3]: {
     name: "Calvin & Hobbes Pale Ale",
     brand: "Watterson Werks",
     description: "Blonde with notes of childhood.",
     alcoholContent: "5%",
     price: 6,
     remainingPints: 124,
-    key: 3,
-    id: 3
+    key: id3,
+    id: id3
   },
-  {
+  [id4]: {
     name: "Goat Lords Cider",
     brand: "Wild Animal Werks",
     description: "Maaahhhhy favorite cider.",
     alcoholContent: "4%",
     price: 5,
     remainingPints: 124,
-    key: 4,
-    id: 4
+    key: id4,
+    id: id4
   },
-  {
+  [id5]: {
     name: "Dinosaur Double IPA",
     brand: "Limited Edition.  Try it before it goes exctinct.",
     description: "RAWR.",
     alcoholContent: "9%",
     price: 8,
     remainingPints: 124,
-    key: 5,
-    id: 5
+    key: id5,
+    id: id5
   },
-  {
+  [id6]: {
     name: "Mary Poppins Kombucha",
     brand: "Sister Suffragettes, inc.",
     description: "Nearly perfect in every way.",
     alcoholContent: "3%",
     price: 7,
     remainingPints: 124,
-    key: 6,
-    id: 6
+    key: id6,
+    id: id6
   }
-]
+}
