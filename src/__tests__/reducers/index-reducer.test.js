12a077e0c3a687a26e72c043e52ad06da9410b8a
Katy Solovewicz <katy@solovewicz.com>
2020-05-02 15:04:37 -0700

    update root reducer test to handle new slices of state

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,8 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+import editingReducer from '../../reducers/editing-reducer';
 
 let store = createStore(rootReducer);
 
@@ -11,7 +13,9 @@ describe("rootReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterKegList: {},
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      selectedKeg: null,
+      editing: false
     });
   });
 
