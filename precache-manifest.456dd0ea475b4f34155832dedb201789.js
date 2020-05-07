3c853c251cd2be75172e9b19ec16458b73a63bb9
Katy Solovewicz <katy@solovewicz.com>
2020-05-07 16:34:17 -0700

    Updates

---
+++ precache-manifest.456dd0ea475b4f34155832dedb201789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4044c6845320d8007abf4c83c229fd1c",
+    "url": "/redux-neumorphic-tap-room/index.html"
+  },
+  {
+    "revision": "eee8e52deee7b1749dd9",
+    "url": "/redux-neumorphic-tap-room/static/css/2.f426b0f3.chunk.css"
+  },
+  {
+    "revision": "878f4bbfc92fea4cc7a2",
+    "url": "/redux-neumorphic-tap-room/static/css/main.121dd35d.chunk.css"
+  },
+  {
+    "revision": "eee8e52deee7b1749dd9",
+    "url": "/redux-neumorphic-tap-room/static/js/2.197d3baf.chunk.js"
+  },
+  {
+    "revision": "878f4bbfc92fea4cc7a2",
+    "url": "/redux-neumorphic-tap-room/static/js/main.1c7b78f1.chunk.js"
+  },
+  {
+    "revision": "286941ea4a59bd077f9c",
+    "url": "/redux-neumorphic-tap-room/static/js/runtime-main.8ee1f6de.js"
+  }
+]);
